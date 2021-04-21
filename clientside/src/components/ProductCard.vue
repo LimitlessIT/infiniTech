105d0f8904e048c04fcede21ce9828cd6950b7b8
ZairiAimenDz <zairiaimendev@gmail.com>
2021-04-21 12:32:47 +0200

    added tailwind

---
+++ clientside/src/components/ProductCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="card">  
+      
+      <div class="image" v-bind:style="{ backgroundImage: 'url(' + photourl + ')' }" />
+      <p href="" class="title">{{ title }}</p>
+      <p><span class="oldprice">{{ oldprice }}</span> <span class="newprice">{{ price }}</span></p>
+  </div>
+</template>
+
+<script>
+import { Number } from "core-js";
+
+export default {
+  name: 'ProductCard',
+  props: {
+      title: String,
+      price: Number,
+      oldprice: Number,
+      onsale: Boolean,
+      photourl: String,
+  }
+}
+</script>
+
+<style scoped>
+  .card:hover{
+    cursor:pointer;
+  }
+  .card{
+    display: flex;
+    height:max-content;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 25px;
+
+    width: 10em;
+    height:20em;
+  }
+  .title{
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 19px;
+    align-self: center;
+    overflow:hidden;
+    text-overflow:ellipsis;
+    display:-webkit-box;
+    -webkit-box-orient:vertical;
+    -webkit-line-clamp:2;
+    margin: 10px 0px;
+  }
+
+  .image{
+      height: 14vh;
+      width:100%;
+      background-position:center;
+      background-repeat: no-repeat;
+      background-size: 100%;
+  }
+
+  .oldprice{
+
+    font-size: 13px;
+    line-height: 140%;
+    align-self:flex-end;
+
+    /* or 20px */
+    text-decoration-line: line-through;
+
+    /* Color - 11 */
+    color: #666666;
+    margin: 10px 0px;
+  }
+.newprice{
+    font-weight:bold;
+    color:#03a;
+    margin: 10px 20px;
+}
+</style>
--- clientside/src/views/Home.vue
+++ clientside/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductCard title="Dell XPS 13, 8GB RAM 256GB NVME SSD With Intel i7 1185G7" price=2.3 oldprice=3 onsale=true photourl="https://www.notebookcheck.biz/fileadmin/Notebooks/News/_nc3/Dell_XPS_13_9310.jpg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld  from '@/components/HelloWorld.vue'
+import ProductCard from '@/components/ProductCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    ProductCard
   }
 }
 </script>
